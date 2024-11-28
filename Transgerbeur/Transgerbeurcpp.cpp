4e8c1c7e59b4193749606dbc224723bafc69d551
matteo <matteoricateau06@gmail.com>
2024-11-28 10:50:54 +0100

    commit #2

--- Transgerbeur/Transgerbeurcpp.cpp
+++ Transgerbeur/Transgerbeurcpp.cpp
@@ -3,6 +3,34 @@
 
 using namespace std;
 
+enum EEtat {vide, palette, rouleau};
+
+struct Spalette {
+	int poids;
+	char reference[20];
+};
+
+struct SRouleau {
+	float longueur;
+	int numero;
+};
+
+
+struct SCasier {
+	int px;
+	int py;
+	contenu cont;
+	EEtat etat;
+};
+
+
+union contenu {
+	Spalette pal;
+	SRouleau roul;
+};
+
+
+
 int main()
 {
 	cout << "Bienvenue dans le Transgerbeur";
