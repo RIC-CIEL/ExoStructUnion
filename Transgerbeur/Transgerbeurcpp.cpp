3c1f32cb431d4cf0490b3687a1caa16b3904223c
matteo <matteoricateau06@gmail.com>
2024-11-28 12:00:23 +0100

    commit #3

--- Transgerbeur/Transgerbeurcpp.cpp
+++ Transgerbeur/Transgerbeurcpp.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 enum EEtat {vide, palette, rouleau};
 
-struct Spalette {
+struct SPalette {
 	int poids;
 	char reference[20];
 };
@@ -25,14 +25,32 @@ struct SCasier {
 
 
 union UContenu {
-	Spalette pal;
+	SPalette pal;
 	SRouleau roul;
 };
 
 
 
-int main()
+void main()
 {
 	cout << "Bienvenue dans le Transgerbeur";
 	SCasier tabCasier[9];
+	SRouleau  ro1;
+	SPalette pa1;
+	UContenu cont1;
+	pa1.poids = 20;
+	strcpy_s(pa1.reference, "The ric veut djin"); // ignisalisation des chaine de caractere
+	EEtat etatC1;
+	etatC1 = vide;
+	cont1.pal = pa1;
+	cont1.roul = ro1;
+	SCasier c1;
+	SCasier c2;
+	c1.cont = cont1; 
+	c1.etat = etatC1;
+	c1.px = 2;
+	c1.py = 3;
+	c2.cont = cont1;
+	tabCasier[0] = c1;
+	tabCasier[2] = c2;
 }
